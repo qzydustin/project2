ea1bc102c5675888a0a04483d351f51de7c04a86
qzydustin <qiz9744@uwec.edu>
2020-04-23 21:10:44 -0500

    add database sql

---
+++ provider/src/make.sql
@@ -0,0 +1,6 @@
+DROP DATABASE IF EXISTS message;
+DROP USER IF EXISTS message_user;
+
+CREATE DATABASE message CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE USER message_user IDENTIFIED BY 'QWERTY123uwec';
+GRANT ALL PRIVILEGES ON message.* TO message_user;
---
+++ provider/src/schema.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS Message;
+CREATE TABLE Message;(
+    id SERIAL PRIMARY KEY,
+    key VARCHAR(20),
+    message TEXT,
+    will_deleted INT default 0,
+    is_deleted INT default 0,
+    day INT,
+    month INT,
+    year INT,
+);
