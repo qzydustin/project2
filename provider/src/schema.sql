8ebd95973cd94e5b1a645ed6b36d2d382c48262a
qzydustin <qiz9744@uwec.edu>
2020-04-23 21:24:46 -0500

    Fix database error

--- provider/src/schema.sql
+++ provider/src/schema.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS Message;
 CREATE TABLE Message(
     id SERIAL PRIMARY KEY,
-    key VARCHAR(20),
+    key CHAR(20),
     message TEXT,
     will_deleted INT default 0,
     is_deleted INT default 0,
