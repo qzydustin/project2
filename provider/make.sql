e3bec2fd9173b4ceb0b93783abdce05b2a442d67
qzydustin <qiz9744@uwec.edu>
2020-05-12 01:02:28 -0500

    Update sql

--- provider/make.sql
+++ provider/make.sql
@@ -3,4 +3,4 @@ DROP USER IF EXISTS orders_user;
 
 CREATE DATABASE orders CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 CREATE USER orders_user IDENTIFIED BY 'QWERTY123uwec!';
-GRANT ALL PRIVILEGES ON orders.* TO message_user;
+GRANT ALL PRIVILEGES ON orders.* TO orders_user;
